b023d1346744ba8281020ca220e33fa052df1154
Ambuj <ambuj@add3.io>
2023-08-10 14:59:34 +0530

    adding imports progressing

---
+++ src/NFTProcessing.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+/// @author @ambuj-k 
+
+/// @title This is a varied range minter contract
+
+pragma solidity ^0.8.18;
+
+import "../ERC712Custom.sol";
+import "../INFT1155Custom.sol";
+
+contract NFTProcessing is ERC712Custom {
+
+
+
+}
+
