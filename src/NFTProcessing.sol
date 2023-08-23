772f07a89b2e5f662841506eba3d97f4de90b5ea
Ambuj <ambuj@add3.io>
2023-08-23 22:31:24 +0530

    Reveal Functionality

--- src/NFTProcessing.sol
+++ src/NFTProcessing.sol
@@ -11,7 +11,27 @@ import "../INFT1155Custom.sol";
 
 contract NFTProcessing is ERC712Custom {
 
+function revealNFT(uint256 tokenId) public {
+        if (!_isApprovedOrOwner(msg.sender, tokenId)){
+            revert Error_Only_NFT_Owner();
+        }
+        if (block.timestamp > startTime){ 
+            revert Error_Ahead_Of_Reveal_Time();
+        }
 
+        string memory metadata = generateMetadata(tokenId);
+        _setTokenURI(tokenId, metadata);
+    }
+
+function generateMetadata(uint256 tokenId) internal view returns (string memory) {
+        // Generate random metadata for the NFT.
+        return "This is the metadata for NFT #" + tokenId.toString();
+    }
+
+modifier onlyAdmin() {
+        if(msg.sender != admin) {revert Error_Only_Owner()};
+        _;
+    }
 
 }
 
