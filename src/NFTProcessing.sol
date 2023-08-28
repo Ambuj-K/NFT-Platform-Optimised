7f1a444d722e5cedd077670aca22f693391fb0b4
Ambuj <ambuj@add3.io>
2023-08-28 23:35:52 +0530

    Changes ongoing 2

--- src/NFTProcessing.sol
+++ src/NFTProcessing.sol
@@ -11,6 +11,9 @@ import "../INFT1155Custom.sol";
 
 contract NFTProcessing is ERC712Custom {
 
+address public owner;
+
+
 // error list
 error Error_Only_NFT_Owner();
 error Error_Ahead_Of_Reveal_Time();
@@ -20,37 +23,37 @@ error Error_Unique_NFT_Mint();
 error Error_Price_Mismatch();
 
 modifier onlyAdmin() {
-        if(msg.sender != admin) {revert Error_Only_Owner()};
-        _;
+    if(msg.sender != admin) {revert Error_Only_Owner()};
+    _;
 }
 
 // TODO Constructor
-constructor (){
-
+constructor (address _owner){
+    owner = _owner;
 }
 
 // mint NFT, for revelataion upon time
 function mintNFT() public payable {
-        if(msg.value != price){ revert Error_Price_Mismatch(); };
-        if(totalSupply() > 1){ revert Error_Unique_NFT_Mint(); };
+    if(msg.value != price){ revert Error_Price_Mismatch(); };
+    if(totalSupply() > 1){ revert Error_Unique_NFT_Mint(); };
 
-        uint256 tokenId = totalSupply() + 1;
-        _mint(msg.sender, tokenId);
-        _setTokenURI(tokenId, "");
+    uint256 tokenId = totalSupply() + 1;
+    _mint(msg.sender, tokenId);
+    _setTokenURI(tokenId, "");
 }
 
 // reveal function 
 function revealNFT(uint256 tokenId) public {
-        if (!_isApprovedOrOwner(msg.sender, tokenId)){
-            revert Error_Only_NFT_Owner();
-        }
-        if (block.timestamp > startTime){ 
-            revert Error_Ahead_Of_Reveal_Time();
-        }
-
-        string memory metadata = generateMetadata(tokenId);
-        _setTokenURI(tokenId, metadata);
+    if (!_isApprovedOrOwner(msg.sender, tokenId)){
+        revert Error_Only_NFT_Owner();
     }
+    if (block.timestamp > startTime){ 
+        revert Error_Ahead_Of_Reveal_Time();
+    }
+
+    string memory metadata = generateMetadata(tokenId);
+    _setTokenURI(tokenId, metadata);
+}
 
 // generate Metadata for NFT
 function generateMetadata(uint256 tokenId) internal view returns (string memory) {
