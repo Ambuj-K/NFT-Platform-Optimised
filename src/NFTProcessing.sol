abb9230377bf83541557ee02cb6f65843be09fae
Ambuj <ambuj@add3.io>
2023-08-29 18:37:40 +0530

    Changes ongoing 3

--- src/NFTProcessing.sol
+++ src/NFTProcessing.sol
@@ -11,54 +11,59 @@ import "../INFT1155Custom.sol";
 
 contract NFTProcessing is ERC712Custom {
 
-address public owner;
+    address public owner;
 
+    // error list
+    error Error_Only_NFT_Owner();
+    error Error_Ahead_Of_Reveal_Time();
+    error Error_Ahead_Of_Reveal_Time();
+    error Error_Only_Owner();
+    error Error_Unique_NFT_Mint();
+    error Error_Price_Mismatch();
 
-// error list
-error Error_Only_NFT_Owner();
-error Error_Ahead_Of_Reveal_Time();
-error Error_Ahead_Of_Reveal_Time();
-error Error_Only_Owner();
-error Error_Unique_NFT_Mint();
-error Error_Price_Mismatch();
-
-modifier onlyAdmin() {
-    if(msg.sender != admin) {revert Error_Only_Owner()};
-    _;
-}
-
-// TODO Constructor
-constructor (address _owner){
-    owner = _owner;
-}
-
-// mint NFT, for revelataion upon time
-function mintNFT() public payable {
-    if(msg.value != price){ revert Error_Price_Mismatch(); };
-    if(totalSupply() > 1){ revert Error_Unique_NFT_Mint(); };
-
-    uint256 tokenId = totalSupply() + 1;
-    _mint(msg.sender, tokenId);
-    _setTokenURI(tokenId, "");
-}
-
-// reveal function 
-function revealNFT(uint256 tokenId) public {
-    if (!_isApprovedOrOwner(msg.sender, tokenId)){
-        revert Error_Only_NFT_Owner();
-    }
-    if (block.timestamp > startTime){ 
-        revert Error_Ahead_Of_Reveal_Time();
+    modifier onlyAdmin() {
+        if(msg.sender != admin) {revert Error_Only_Owner()};
+        _;
     }
 
-    string memory metadata = generateMetadata(tokenId);
-    _setTokenURI(tokenId, metadata);
-}
+    // TODO Constructor
+    constructor (address _owner){
+        owner = _owner;
+    }
 
-// generate Metadata for NFT
-function generateMetadata(uint256 tokenId) internal view returns (string memory) {
+    // mint NFT, for revelataion upon time
+    function mintNFT() public payable {
+        if(msg.value != price){ revert Error_Price_Mismatch(); };
+        if(totalSupply() > 1){ revert Error_Unique_NFT_Mint(); };
+
+        uint256 tokenId = totalSupply() + 1;
+        _mint(msg.sender, tokenId);
+        _setTokenURI(tokenId, "");
+    }
+
+    // reveal function 
+    function revealNFT(uint256 tokenId) public {
+        if (!_isApprovedOrOwner(msg.sender, tokenId)){
+            revert Error_Only_NFT_Owner();
+        }
+        if (block.timestamp > startTime){ 
+            revert Error_Ahead_Of_Reveal_Time();
+        }
+
+        string memory metadata = generateMetadata(tokenId);
+        _setTokenURI(tokenId, metadata);
+    }
+
+    // generate Metadata for NFT
+    function generateMetadata(uint256 tokenId) internal view returns (string memory) {
         // Generate random metadata for the NFT.
         return "This is the metadata for NFT #" + tokenId.toString();
+    }
+
+    // withdraw funds, more TODO
+    function withdrawFunds(address _to) public onlyAdmin {
+        uint256 totalFunds = totalSupply() * price;
+        _transfer(address(0), _to, totalFunds);
     }
 
 }
---
+++ test/NFT1155CustomTest.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+//@author @ambuj-k
+
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import "../src/NFT1155Custom.sol";
+import "../src/NFTProcessing.sol";
+
+contract NFTProcessingTest is Test {
+    NFT1155Custom tokenObj;
+    NFTProcessing minterObj;
+
+    function setUp() public {
+        alice = makeAddr("Alice");
+        bob = makeAddr("Bob");
+
+        usd.mint(alice, 1000 ether);
+        usd.mint(bob, 1000 ether);
+        eur.mint(alice, 1000 ether);
+        eur.mint(bob, 1000 ether);
+        tokenObj = new NFT1155Custom("LimRel", 2, 45 days);
+        tokenObj.setOWner(address(0));
+        minterObj = new NFTProcessing("Minter");
+        minterObj.setOWner(address(0));
+    }
+
+}
